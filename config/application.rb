a518fcd334eb2485ae49087d2fc1317d3a1846ad
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-22 12:27:10 +0900

    feat: 英語に変更

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module HangulAppRails
     # in config/environments, which are processed later.
     #
     config.time_zone = 'Tokyo'
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
 
     # config.eager_load_paths << Rails.root.join("extras")
