bf2f66c0d3ff30d3307b549aab271f7ce7c9fdd8
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-24 16:01:21 +0900

    feat: /courses/:id

--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Courses", type: :request do
-  describe "GET /index" do
+  describe "GET /courses" do
     it "returns a successful response" do
       get api_v1_courses_path
       expect(response).to have_http_status(:success)
@@ -14,4 +14,20 @@ RSpec.describe "Api::V1::Courses", type: :request do
       expect(json.size).to eq(3)
     end
   end
+
+  describe "GET /courses/:id" do
+    let(:course) { create(:course) }
+
+    it "returns a successful response" do
+      get api_v1_course_path(course)
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns NotFound when course not found" do
+      get api_v1_course_path(9999)
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+
 end
