37df0faa87c8a9b8c39ffc10eb422978b0839356
hulk-higakijin <hm385.chejptks@gmail.com>
2024-07-24 16:03:25 +0900

    fix: testの修正

--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Courses", type: :request do
   describe "GET /courses" do
+    before do
+      create_list(:course, 3)
+    end
+
     it "returns a successful response" do
       get api_v1_courses_path
       expect(response).to have_http_status(:success)
     end
 
     it "returns a list of courses" do
-      create_list(:course, 3)
       get api_v1_courses_path
       json = JSON.parse(response.body)
       expect(json.size).to eq(3)
