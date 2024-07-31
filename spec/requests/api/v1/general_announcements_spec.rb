3ac37b32553687be7d7dac3e69e9d1b0a6938df3
higakijin <higakijin@higakijins-MacBook-Air.local>
2024-08-01 00:02:08 +0900

    test: request spec

--- spec/requests/api/v1/general_announcements_spec.rb
+++ spec/requests/api/v1/general_announcements_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::GeneralAnnouncements", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::GeneralAnnouncements', type: :request do
+  let!(:genral_announcement) { create(:general_announcement) }
+
+  describe 'GET /' do
+    before { get '/api/v1/general_announcements' }
+
+    it 'returns general announcements' do
+      expect(response.body).to include(genral_announcement.title)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /:id' do
+    before { get "/api/v1/general_announcements/#{genral_announcement.id}" }
+
+    it 'returns general announcement' do
+      expect(response.body).to include(genral_announcement.title)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
 end
