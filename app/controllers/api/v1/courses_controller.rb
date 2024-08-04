2d051c0e45536893196b762b5553ebce3fe6e5b7
higakijin <higakijin@higakijins-MacBook-Air.local>
2024-08-04 20:16:29 +0900

    feat: add choices

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -5,6 +5,18 @@ class Api::V1::CoursesController < ApplicationController
 
   def show
     @course = Course.find(params[:id])
-    @words = @course.words
+    all_words = @course.words.to_a
+    @words = all_words.map do |word|
+      dummy_words = all_words.reject { |w| w.id == word.id }.shuffle.take(3)
+      word.attributes.symbolize_keys.merge(choices: generate_choices([word, *dummy_words]))
+    end
   end
+
+  private
+
+    def generate_choices(words)
+      words.map do |word|
+        word.attributes.symbolize_keys.merge(is_correct: word.id == words.first.id)
+      end
+    end
 end
--- app/views/api/v1/courses/show.json.jbuilder
+++ app/views/api/v1/courses/show.json.jbuilder
@@ -1,4 +1,5 @@
 json.partial! 'api/v1/courses/course', course: @course
 json.words @words do |word|
-  json.partial! 'api/v1/words/word', word: word
+  json.word word 
 end
+
