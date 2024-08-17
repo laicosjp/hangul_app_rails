6eacb975beef5c30f1f820e04a83b22c88f49589
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-17 16:13:05 +0900

    chore: kaminari

--- app/controllers/api/v1/courses/words_controller.rb
+++ app/controllers/api/v1/courses/words_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Courses::WordsController < ApplicationController
   before_action :set_course
 
   def show
-    @words = @course.words.includes(:choices)
+    @words = @course.words.page(params[:page]).per(params[:per_page])
   end
 
   private
