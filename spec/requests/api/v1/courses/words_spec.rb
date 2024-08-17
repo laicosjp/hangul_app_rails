e2bdb02f13202fe8df2e102db37d7a7bb4dd627d
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-17 16:18:00 +0900

    test: Api::V1::Courses::Words

--- spec/requests/api/v1/courses/words_spec.rb
+++ spec/requests/api/v1/courses/words_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Courses::Words", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:course) { create(:course) }
+  let(:words) { create_list(:word, 50, course: course) }
+
+  describe "GET /" do
+    it "returns http success" do
+      get api_v1_course_words_path(course_id: course.id)
+      expect(response).to have_http_status(:success)
+    end
   end
 end
