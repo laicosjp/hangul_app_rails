eef37b38ef7ef5049d7e177bdf734c887d3e92bc
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-19 17:49:53 +0900

    feat: add correctWordsCount in courses#show

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,10 +1,16 @@
+# typed: true
+
 class Api::V1::CoursesController < ApplicationController
+  before_action :authenticate_user!, only: :show
+
   def index
     @courses = Course.all
   end
 
   def show
     @course = Course.find(params[:id])
-    @words = @course.words.includes(:choices).first(Word::EXAMPLE_WORDS_COUNT)
+    @example_words = @course.words.includes(:choices).first(Word::EXAMPLE_WORDS_COUNT)
+    @corrected_words_count = @course.words.selected_by_records(user_id: current_user.id, status: 'correct').length
+    @incorrected_words_count = @course.words.selected_by_records(user_id: current_user.id, status: 'incorrect').length
   end
 end
