cd7d7488a509ffd436fea66ef9b12140c1ebbc95
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-19 19:00:43 +0900

    feat: unstudied_words_count

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -12,5 +12,6 @@ class Api::V1::CoursesController < ApplicationController
     @example_words = @course.words.includes(:choices).first(Word::EXAMPLE_WORDS_COUNT)
     @corrected_words_count = @course.words.selected_by_records(user_id: current_user.id, status: 'correct').length
     @incorrected_words_count = @course.words.selected_by_records(user_id: current_user.id, status: 'incorrect').length
+    @unstudied_words_count = @course.words_count - (@corrected_words_count + @incorrected_words_count)
   end
 end
