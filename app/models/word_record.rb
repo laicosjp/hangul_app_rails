3ec7bd8ba6152475afab731c1790440ec2657925
hulk-higakijin <hm385.chejptks@gmail.com>
2024-08-24 17:51:29 +0900

    feat: add step's enum in model

--- app/models/word_record.rb
+++ app/models/word_record.rb
@@ -29,6 +29,25 @@ class WordRecord < ApplicationRecord
   belongs_to :word
 
   enum status: { correct: 1, incorrect: 2 }
+  enum step: {
+    # This integer value is based on the key name. The unit is minute.
+    # ex)
+    #  one_minute_later is 1.
+    #  one_hour_later is 60,
+    zero: 0,
+    one_minute_later: 1,
+    ten_minutes_later: 10,
+    thirty_minutes_later: 30,
+    one_hour_later: 60,
+    seven_hour_later: 420,
+    one_day_later: 1440,
+    three_days_later: 4320,
+    ten_days_later: 14_400,
+    thirty_days_later: 43_200,
+    fourty_five_days_later: 64_800,
+    sixty_days_later: 86_400,
+    ninety_days_later: 129_600
+  }
 
   validates :studied_at, presence: true
   validates :word_id, uniqueness: { scope: :user_id }
