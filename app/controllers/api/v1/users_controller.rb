5b6add383e5c4997be44ed319954bf5f61b0206c
hulk-higakijin <hm385.chejptks@gmail.com>
2024-11-11 19:20:37 +0900

    fix: routing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,20 +4,12 @@ class Api::V1::UsersController < ApplicationController
   # DO NOT USE devise_token_auth's controller when user leaves the app.
   # Because devise_token_auth's controller delete user's information completely from database.
   def destroy
-    return nil unless @user == current_user
-
-    params = { left_at: Time.current, email: "alda+#{@user.id}@example.com", uid: "alda+#{@user.id}@example.com" }
+    params = { left_at: Time.current, email: "alda+#{current_user.id}@example.com", uid: "alda+#{current_user.id}@example.com" }
 
     if current_user.update!(params)
-      render json: { status: 'success', message: "User id: #{@user.id} has left the app." }
+      render json: { status: 'success', message: "User id: #{current_user.id} has left the app." }
     else
       render json: { status: 'error', message: 'Failed to leave the app.' }, status: :internal_server_error
     end
   end
-
-  private
-
-    def set_user
-      @user = User.find(params[:id])
-    end
 end
