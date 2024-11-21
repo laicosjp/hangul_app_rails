566767c4eb124bd135d6fdb516c8d61e81c6e376
hulk-higakijin <hm385.chejptks@gmail.com>
2024-11-21 18:46:12 +0900

    test: add test for /api/v2/courses & /api/v2/courses/:course_id

--- spec/requests/api/v2/courses_spec.rb
+++ spec/requests/api/v2/courses_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V2::Courses", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:course) { create_list(:course, 10) }
+
+  describe "GET /courses" do
+    it "returns a success response" do
+      get api_v2_courses_path
+      expect(response).to be_successful
+      expect(JSON.parse(response.body).length).to eq(10)
+    end
+  end
+
+  describe "GET /courses/:id" do
+    it "returns a success response" do
+      get api_v2_course_path(course.first)
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)["id"]).to eq(course.first.id)
+    end
   end
 end
