3a8a52722c9c4d0a3f7d22f56fe127cebfa3aa2f
hulk-higakijin <hm385.chejptks@gmail.com>
2024-11-25 17:29:29 +0900

    chore: remove rails_admin & rails_admin_import

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
