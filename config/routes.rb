1b463f12b2b3d71d3521b307dd49285411362908
hulk-higakijin <hm385.chejptks@gmail.com>
2024-11-25 17:46:43 +0900

    feat: Home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'home#index'
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
